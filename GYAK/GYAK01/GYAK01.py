701e8bb023ffc6b22fcc607cbbad884531fa25c0
anderudp <92269603+anderudp@users.noreply.github.com>
2023-02-28 23:59:05 +0100

    gyak 1 done

---
+++ GYAK/GYAK01/GYAK01.py
@@ -0,0 +1,79 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[3]:
+
+
+#Create a function that decides if a list contains any odd numbers.
+#return type: bool
+#function name must be: contains_odd
+#input parameters: input_list
+
+
+# In[7]:
+
+
+def contains_odd(input_list) -> bool:
+    for num in input_list:
+        if num % 2 == 1:
+            return True
+    return False
+
+
+# In[6]:
+
+
+#Create a function that accepts a list of integers, and returns a list of bool.
+#The return list should be a "mask" and indicate whether the list element is odd or not.
+#(return should look like this: [True,False,False,.....])
+#return type: list
+#function name must be: is_odd
+#input parameters: input_list
+
+
+# In[ ]:
+
+
+def is_odd(input_list) -> list:
+    return [False if num % 2 == 0 else True for num in input_list]
+
+
+# In[ ]:
+
+
+#Create a function that accpects 2 lists of integers and returns their element wise sum. <br>
+#(return should be a list)
+#return type: list
+#function name must be: element_wise_sum
+#input parameters: input_list_1, input_list_2
+
+
+# In[ ]:
+
+
+def element_wise_sum(input_list_1, input_list_2) -> list:
+    return [input_list_1[i] + input_list_2[i] for i in range(len(input_list_1))]
+
+
+# In[ ]:
+
+
+#Create a function that accepts a dictionary and returns its items as a list of tuples
+#(return should look like this: [(key,value),(key,value),....])
+#return type: list
+#function name must be: dict_to_list
+#input parameters: input_dict
+
+
+# In[ ]:
+
+
+def dict_to_list(input_dict) -> list:
+    return input_dict.items()
+
+
+# In[ ]:
+
+
+#If all the functions are created convert this notebook into a .py file and push to your repo
+
