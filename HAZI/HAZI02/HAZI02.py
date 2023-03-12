f10f4bea056aa7c476f85d5884f9e8e399611f67
anderudp <92269603+anderudp@users.noreply.github.com>
2023-03-12 13:14:29 +0100

    [UPDATE] hw2 done

--- HAZI/HAZI02/HAZI02.py
+++ HAZI/HAZI02/HAZI02.py
@@ -42,4 +42,81 @@ def encode_Y(input_arr, class_count) -> np.array:
 # Ki:  [1, 2, 0, 3]
 # decode_Y()
 def decode_Y(input_arr) -> np.array:
-    
+    return np.argmax(input_arr, axis=1)
+
+
+# Készíts egy olyan függvényt, ami képes kiértékelni egy neurális háló eredményét! Bemenetként egy listát és egy array-t és adja vissza azt az elemet, aminek a legnagyobb a valószínüsége(értéke) a listából.
+# Be: ['alma', 'körte', 'szilva'], [0.2, 0.2, 0.6]. # Az ['alma', 'körte', 'szilva'] egy lista!
+# Ki: 'szilva'
+# eval_classification()
+def eval_classification(labels: list, probabilities: np.array):
+    return labels[np.argmax(probabilities)]
+
+
+# Készíts egy olyan függvényt, ahol az 1D array-ben a páratlan számokat -1-re cseréli
+# Be: [1,2,3,4,5,6]
+# Ki: [-1,2,-1,4,-1,6]
+# replace_odd_numbers()
+def replace_odd_numbers(input_arr: np.array) -> np.array:
+    input_arr[input_arr % 2 != 0] = -1
+    return input_arr
+
+
+# Készíts egy olyan függvényt, ami egy array értékeit -1 és 1-re változtatja, attól függően, hogy az adott elem nagyobb vagy kisebb a paraméterként megadott számnál.
+# Ha a szám kisebb mint a megadott érték, akkor -1, ha nagyobb vagy egyenlő, akkor pedig 1.
+# Be: [1, 2, 5, 0], 2
+# Ki: [-1, 1, 1, -1]
+# replace_by_value()
+def replace_by_value(input_arr: np.array, value) -> np.array:
+    input_arr[input_arr < value] = -1
+    input_arr[input_arr >= value] = 1
+    return input_arr
+
+
+# Készíts egy olyan függvényt, ami egy array értékeit összeszorozza és az eredményt visszaadja
+# Be: [1,2,3,4]
+# Ki: 24
+# array_multi()
+# Ha több dimenziós a tömb, akkor az egész tömb elemeinek szorzatával térjen vissza
+def array_multi(input_arr: np.array):
+    return np.prod(input_arr)
+
+
+# Készíts egy olyan függvényt, ami egy 2D array értékeit összeszorozza és egy olyan array-el tér vissza, aminek az elemei a soroknak a szorzata
+# Be: [[1, 2], [3, 4]]
+# Ki: [2, 12]
+# array_multi_2d()
+def array_multi_2d(input_arr: np.array):
+    return np.prod(input_arr, axis=1)
+
+
+# Készíts egy olyan függvényt, amit egy meglévő numpy array-hez készít egy bordert nullásokkal. Bementként egy array-t várjon és kimenetként egy array jelenjen meg aminek van border-je
+# Be: [[1,2],[3,4]]
+# Ki: [[0,0,0,0],[0,1,2,0],[0,3,4,0],[0,0,0,0]]
+# add_border()
+def add_border(input_arr: np.array):
+    return np.pad(input_arr, pad_width=1, mode="constant", constant_values=0)
+
+
+# Készíts egy olyan függvényt ami két dátum között felsorolja az összes napot és ezt adja vissza egy numpy array-ben. A fgv ként str vár paraméterként 'YYYY-MM' formában.
+# Be: '2023-03', '2023-04'  # mind a kettő paraméter str.
+# Ki: ['2023-03-01', '2023-03-02', .. , '2023-03-31',]
+# list_days()
+def list_days(date1: str, date2: str) -> np.array:
+    return np.arange(date1, date2, dtype="datetime64[D]")
+
+
+# Írj egy fügvényt ami vissza adja az aktuális dátumot az alábbi formában: YYYY-MM-DD. Térjen vissza egy 'numpy.datetime64' típussal.
+# Be:
+# Ki: 2017-03-24
+# get_act_date()
+def get_act_date() -> np.datetime64:
+    return np.datetime64('today')
+
+
+# Írj egy olyan függvényt ami visszadja, hogy mennyi másodperc telt el 1970 január 01. 00:02:00 óta. Int-el térjen vissza
+# Be:
+# Ki: másodpercben az idó, int-é kasztolva
+# sec_from_1970()
+def sec_from_1970():
+    return (np.datetime64('now') - np.datetime64('1970-01-01T00:02:00')).astype(int)
