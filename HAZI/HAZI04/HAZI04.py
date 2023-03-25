4e79f8ece3daaa056514d0a493b25cbcca702f82
anderudp <92269603+anderudp@users.noreply.github.com>
2023-03-25 14:35:01 +0100

    [UPDATE] hazi04 done

--- HAZI/HAZI04/HAZI04.py
+++ HAZI/HAZI04/HAZI04.py
@@ -1,11 +1,7 @@
 import random
-
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
-
-pd.set_option('display.max_columns', None)
-pd.set_option('display.max_rows', None)
 
 def csv_to_df(csv_path: str) -> pd.DataFrame:
     return pd.read_csv(csv_path)
@@ -47,4 +43,48 @@ def female_top_score(input_df: pd.DataFrame) -> tuple:
     new_df = new_df[new_df['score total'] == new_df['score total'].max()]
     return new_df['math score'].iloc[0], new_df['reading score'].iloc[0], new_df['writing score'].iloc[0]
 
-print(female_top_score(csv_to_df("StudentsPerformance.csv")))
+
+def add_grade(input_df: pd.DataFrame) -> pd.DataFrame:
+    new_df = input_df.copy()
+    new_df['grade'] = new_df[["math score", "reading score", "writing score"]].sum(axis=1).div(300)
+    new_df['grade'] = new_df['grade'].apply(lambda x:
+                                            'A' if x >= 0.9
+                                            else 'B' if x >= 0.8
+                                            else 'C' if x >= 0.7
+                                            else 'D' if x >= 0.6
+                                            else 'F')
+    return new_df
+
+
+def math_bar_plot(input_df: pd.DataFrame) -> plt.Figure:
+    new_df = input_df.copy()
+    new_df = new_df.groupby(['gender']).mean(numeric_only=True)['math score']
+    fig, ax = plt.subplots()
+    ax = new_df.plot(kind="bar")
+    ax.set_title('Average Math Score by Gender')
+    ax.set_xlabel('Gender')
+    ax.set_ylabel('Math Score')
+    #plt.show()
+    return fig
+
+
+def writing_hist(input_df: pd.DataFrame) -> plt.Figure:
+    new_df = input_df.copy()
+    fig, ax = plt.subplots()
+    ax.hist(new_df['writing score'])
+    ax.set_title('Distribution of Writing Scores')
+    ax.set_xlabel('Writing Score')
+    ax.set_ylabel('Number of Students')
+    #plt.show()
+    return fig
+
+
+def ethnicity_pie_chart(input_df: pd.DataFrame) -> plt.Figure:
+    new_df = input_df.copy()
+    new_df = new_df.groupby(['race/ethnicity']).size().reset_index(name="counts")
+    print(new_df)
+    fig, ax = plt.subplots()
+    ax.pie(new_df["counts"], autopct='%1.1f%%', labels=new_df['race/ethnicity'])
+    ax.set_title('Proportion of Students by Race/Ethnicity')
+    plt.show()
+    return fig
